738fd4a77f383ac924f20fa58e30eb7acb3d82ad
DyuvenzhiMaxim <Tyuiu.DyuvenzhiMI@outlook.com>
2025-09-14 01:46:20 +0500

    Task1

---
+++ Tyuiu.DyuvenzhiMI.Sprint0.Task1.V0.Lib/Class1.cs
@@ -0,0 +1,7 @@
+﻿namespace Tyuiu.DyuvenzhiMI.Sprint0.Task1.V0.Lib
+{
+    public class Class1
+    {
+
+    }
+}
---
+++ ﻿Tyuiu.DyuvenzhiMI.Sprint0.Task1.V0/Program.cs
@@ -0,0 +1,12 @@
+﻿
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World");
+        string name = "Максим";
+        int age = 18;
+        Console.WriteLine("Здравствуйте, меня зовут " + name + " мне " + age + " лет!");
+        Console.ReadKey();
+    }
+}
