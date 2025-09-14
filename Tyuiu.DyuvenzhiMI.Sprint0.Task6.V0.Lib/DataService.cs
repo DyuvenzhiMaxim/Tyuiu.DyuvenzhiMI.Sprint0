1314563e24801e593a7a15c34d29b4338868bdd6
DyuvenzhiMaxim <Tyuiu.DyuvenzhiMI@outlook.com>
2025-09-14 23:25:49 +0500

    Task6

---
+++ Tyuiu.DyuvenzhiMI.Sprint0.Task6.V0.Lib/DataService.cs
@@ -0,0 +1,44 @@
+﻿namespace Tyuiu.DyuvenzhiMI.Sprint0.Task6.V0.Lib
+{
+    public class DataService
+    {
+        //Пример циклической структуры (цикл с параметром) for
+        public static object AdditionArray(int[] numbers)
+        {
+            var total = 0;
+            for (var i = 0; i < numbers.Length; i++)
+            {
+                total += numbers[i];
+            }
+            return total;
+        }
+
+        //Пример циклической структуры (цикл с параметром) while
+        public static object SubtractionArray(int[] numbers)
+        {
+            var total = 0;
+            int index = 0;
+            while (index < numbers.Length)
+            {
+                total -= numbers[index];
+                index++;
+            }
+            return total;
+        }
+
+        //Пример циклической структуры (цикл с параметром) do while
+        public static object MultiplicationArray(int[] numbers)
+        {
+            var total = 1;
+            int index = 0;
+            do
+            {
+                total *= numbers[index];
+                index++;
+            }
+            while (index < numbers.Length);
+
+            return total;
+        }
+    }
+}
---
+++ Tyuiu.DyuvenzhiMI.Sprint0.Task6.V0/Program.cs
@@ -0,0 +1,19 @@
+﻿using Tyuiu.DyuvenzhiMI.Sprint0.Task6.V0.Lib;
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        int[] numsArray = new int[] { 1, 2, 3, 4, 5 };
+
+        //Пример циклического алгоритма находящийся в библиотеке классов в методе AdditionArray
+        Console.WriteLine("Сумма элементов массива = " + DataService.AdditionArray(numsArray));
+
+        //Пример циклического алгоритма находящийся в библиотеке классов в методе SubtractionArray
+        Console.WriteLine("Сумма элементов массива = " + DataService.SubtractionArray(numsArray));
+
+        //Пример циклического алгоритма находящийся в библиотеке классов в методе MultiplicationArray
+        Console.WriteLine("Сумма элементов массива = " + DataService.MultiplicationArray(numsArray));
+
+        Console.ReadKey();
+    }
+}
