c5ca55f6252d3b1b11e771fc521a3a44ee1d091b
DyuvenzhiMaxim <Tyuiu.DyuvenzhiMI@outlook.com>
2025-09-14 22:37:01 +0500

    Task5

---
+++ Tyuiu.DyuvenzhiMI.Sprint0.Task5.V0.Lib/DataService.cs
@@ -0,0 +1,39 @@
+﻿namespace Tyuiu.DyuvenzhiMI.Sprint0.Task5.V0.Lib
+{
+    public class DataService
+    {
+        //Пример линейной структуры
+        public static int Addition(int a, int b)
+        {
+            return a + b;
+        }
+
+        //Пример линейной структуры
+        public static int Subtraction(int a, int b)
+        {
+            return a - b;
+        }
+
+        //Пример линейной структуры
+        public static int Multiplication(int a, int b)
+        {
+            return a * b;
+        }
+
+        //Пример линейной структуры
+        public static int Division(int a, int b)
+        {
+            //Пример только демонстративный
+            //В реальных проектах лучше использовать Exception  
+            if (b == 0)
+            {
+                Console.WriteLine("Переменная b = {0}, на ноль делить нельзя!", b);
+                return -1;
+            }
+            else
+            {
+                return a / b;
+            }
+        }
+    }
+}
---
+++ Tyuiu.DyuvenzhiMI.Sprint0.Task5.V0/Program.cs
@@ -0,0 +1,16 @@
+﻿using Tyuiu.DyuvenzhiMI.Sprint0.Task5.V0.Lib;
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        //Пример линейной структуры
+        Console.WriteLine("A + B = " + DataService.Addition(1, 5));
+        Console.WriteLine("A - B = " + DataService.Subtraction(15, 5));
+        Console.WriteLine("A * B = " + DataService.Multiplication(10, 10));
+
+        //Пример разветвляющейся структуры в библиотеке классов в методе Division
+        Console.WriteLine("A / B = " + DataService.Division(5, 0));
+
+        Console.ReadKey();
+    }
+}
